e0cd4622e3e2635aa8e52af1c28ac9a8e840055d
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-26 19:39:23 -0800

    Upgrade modules

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,2 @@
-export interface LinkOptions {
-  cacheDirectory?: string;
-}
 export type LinkCallback = (err?: Error, installedAt?: string) => void;
-
-export interface UnLinkOptions {
-  cacheDirectory?: string;
-}
 export type UnlinkCallback = (err?: Error, installedAt?: string) => void;
