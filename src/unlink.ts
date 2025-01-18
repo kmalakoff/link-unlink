c2576252da074e744123f445696dab454ec47ac1
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-17 17:47:18 -0800

    Upgrade modules

--- src/unlink.ts
+++ src/unlink.ts
@@ -1,7 +1,10 @@
 import fs from 'fs';
 import path from 'path';
+import { Lock } from 'lock';
 import Queue from 'queue-cb';
 import rimraf2 from 'rimraf2';
+
+const lock = Lock();
 
 function restoreLink(previous, target, callback) {
   rimraf2(target, { disableGlob: true }, (err) => {
@@ -10,30 +13,34 @@ function restoreLink(previous, target, callback) {
 }
 
 function worker(target, callback) {
-  const dirname = path.dirname(target);
-  const basename = path.basename(target);
+  lock(target, (release) => {
+    callback = release(callback);
 
-  fs.readdir(dirname, (err, files) => {
-    if (err) return callback(err);
-    const matches = files.filter((x) => x.indexOf(basename) === 0 && x.slice(basename.length)[0] === '.').map((x) => path.join(dirname, x));
-    if (matches.length === 0) return rimraf2(target, { disableGlob: true }, callback);
-    if (matches.length === 1) return restoreLink(matches[0], target, callback);
+    const dirname = path.dirname(target);
+    const basename = path.basename(target);
 
-    const stats = [];
-    const queue = new Queue();
-    matches.forEach((match) =>
-      queue.defer((cb) => {
-        fs.lstat(match, (err, stat) => {
-          if (err) return callback(err);
-          stats.push({ match, stat });
-          cb();
-        });
-      })
-    );
-    queue.await((err) => {
+    fs.readdir(dirname, (err, files) => {
       if (err) return callback(err);
-      const sorted = stats.sort((a, b) => b.stat.ctime.valueOf() - a.stat.ctime.valueOf());
-      restoreLink(sorted[0].match, target, callback);
+      const matches = files.filter((x) => x.indexOf(basename) === 0 && x.slice(basename.length)[0] === '.').map((x) => path.join(dirname, x));
+      if (matches.length === 0) return rimraf2(target, { disableGlob: true }, callback);
+      if (matches.length === 1) return restoreLink(matches[0], target, callback);
+
+      const stats = [];
+      const queue = new Queue();
+      matches.forEach((match) =>
+        queue.defer((cb) => {
+          fs.lstat(match, (err, stat) => {
+            if (err) return callback(err);
+            stats.push({ match, stat });
+            cb();
+          });
+        })
+      );
+      queue.await((err) => {
+        if (err) return callback(err);
+        const sorted = stats.sort((a, b) => b.stat.ctime.valueOf() - a.stat.ctime.valueOf());
+        restoreLink(sorted[0].match, target, callback);
+      });
     });
   });
 }
